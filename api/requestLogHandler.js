1963e23d83318e1e084127b681e55354e2afe6b2
acharyarajasekhar <acharya.r@live.com>
2016-07-05 00:18:20 +0530

    changes

--- api/requestLogHandler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/*
-Handler that logs request url and client ip to console 
-*/
-
-module.exports = function authorizationHandler(req, res, next) {
-    console.log('request from ip "' + req.ip + '" to url "' + req.url + '" at ' + new Date());
-    next();
-}
