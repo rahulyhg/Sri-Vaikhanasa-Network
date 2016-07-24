7c25859bc771eb85cbc1a7defe1786b02b5298ba
acharyarajasekhar <acharya.r@live.com>
2016-07-24 16:13:48 +0530

    unit test issue fix

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ before(function (done) {
     app = require('../server');
     app.on("appServerStarted", function () {
         app.on("dbServerConnected", function () {
-            console.log('I am up and running...');
+            console.log('I am up and running @ ' + app.webServer);
             done();
         });
     });    
