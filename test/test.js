9f5525c2bb2c4576dfaadbac42b37d6860d3d29a
acharyarajasekhar <acharya.r@live.com>
2016-07-24 15:23:14 +0530

    unit test fix

--- test/test.js
+++ test/test.js
@@ -6,13 +6,11 @@ var supertest = require('supertest');
 var app = require('../server');
 var api = supertest(app);
 before(function (done) {
-    if (app) {
-        app.on("appServerStarted", function () {
-            app.on("dbServerConnected", function () {
-                done();
-            });
+    app.on("appServerStarted", function () {
+        app.on("dbServerConnected", function () {
+            done();
         });
-    }
+    });
 });
 
 // var api = supertest('http://localhost:3000');
