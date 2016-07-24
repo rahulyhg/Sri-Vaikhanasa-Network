785c071fe34848c312a12d7c6739d4af49ca848b
acharyarajasekhar <acharya.r@live.com>
2016-07-24 15:48:50 +0530

    Fixing travis ci issues

--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@ var api = supertest(app);
 before(function (done) {
     app.on("appServerStarted", function () {
         app.on("dbServerConnected", function () {
+            console.log('I am up and running...');
             done();
         });
     });
