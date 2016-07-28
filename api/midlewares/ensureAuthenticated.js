e48e6354189116232617705e7b1dbd20f8e3bf9c
acharyarajasekhar <acharya.r@live.com>
2016-07-28 23:55:11 +0530

    code review fix

--- api/midlewares/ensureAuthenticated.js
+++ api/midlewares/ensureAuthenticated.js
@@ -1,9 +1,8 @@
 "use strict";
 
 module.exports = function ensureAuthenticated(req, res, next) {
-    console.log("is auth: " + req.isAuthenticated());
     if (req.isAuthenticated()) {
         return next();
     }
     return res.redirect("/auth/google");
-}
+};
--- test/oauth.js
+++ test/oauth.js
@@ -1,43 +1,49 @@
 "use strict";
 
 // Dependency modules
-var WalkingDead = require('walking-dead');
-var cobbler = require('cobbler');
+var WalkingDead = require("walking-dead");
+var cobbler = require("cobbler");
 
-module.exports = function(api, expect) {
+module.exports = function (api, expect) {
 
-    describe("Google OAuth Test", function() {
+    describe("Google OAuth Test", function () {
         var url = "https://sri-vaikhanasa-network-acharyarajasekhar.c9users.io/auth/google";
         var zopts = {
             debug: false,
             silent: false
         };
         var googleProfile = {
-            provider: 'google',
-            id: 'abcdef',
-            displayName: 'John Doe',
-            email: 'john@doe.com'
+            provider: "google",
+            id: "abcdef",
+            displayName: "John Doe",
+            email: "john@doe.com"
         };
 
-        it("Mocked google oauth login", function(done) {
+        it("Mocked google oauth login", function (done) {
             var GoogleStrategy = require("passport-google-oauth2").Strategy;
             var passport = cobbler(GoogleStrategy, googleProfile, {
-                callbackURL: '/oauth2callback'
+                callbackURL: "/oauth2callback"
             });
-            new WalkingDead(url).zombify(zopts)
+            new WalkingDead(url).zombify(zopts);
             api
                 .get("/auth/google")
-                .end(function(error, response) {
+                .end(function (error, response) {
                     expect(error).to.be.a.null;
                     expect(response.statusCode).to.equal(302);
                     expect(response.headers.location).to.contain("/oauth2callback?code=12345");
                     api
                         .get("/oauth2callback?code=12345")
-                        .end(function(error, response) {
+                        .end(function (error, response) {
                             expect(error).to.be.a.null;
                             expect(response.statusCode).to.equal(302);
                             expect(response.headers.location).to.contain("/auth/google/success");
-                            done(error);
+                            api
+                                .get(response.headers.location)
+                                .end(function (error, response) {
+                                    expect(error).to.be.a.null;
+                                    expect(response.statusCode).to.equal(200);
+                                    done(error);
+                                });
                         });
                 });
         });
