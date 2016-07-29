289efd0b14d4c217f8bddc340d3cee132608a7c9
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2016-07-29 14:21:27 +0000

    code review fix

--- api/core/setAppConfig.js
+++ api/core/setAppConfig.js
@@ -3,30 +3,26 @@
 // Module dependencies
 var config = require("config");
 
-module.exports = function (app) {
-    app.set("host", getHost());
-    app.set("port", getPort());
-    app.set("connectionString", getDbConnUr());
-    app.set("tokenPrivateKey", getSecretForToken());
+module.exports = function(app) {
+    app.set("host", coalesce(process.env.OPENSHIFT_NODEJS_IP, process.env.IP, config.appServer.host));
+    app.set("port", coalesce(process.env.OPENSHIFT_NODEJS_PORT, process.env.PORT, config.appServer.port));
+    app.set("connectionString", coalesce(process.env.SVNDB_URL, config.dbServer.url));
+    app.set("tokenPrivateKey", coalesce(process.env.SVN_SECRET_CODE, config.tokenPrivateKey));
     app.set("tokenExpiresIn", config.tokenExpiresIn);
     app.set("googleOAuthClientID", process.env.SVN_GOOGLE_CLIENTID);
     app.set("googleOAuthClientSecret", process.env.SVN_GOOGLE_CLIENTSECRET);
     app.set("googleOAuthCallbackUrl", process.env.SVN_GOOGLE_CALLBACK_URL);
 };
 
-var getHost = function(){
-    return process.env.OPENSHIFT_NODEJS_IP || process.env.IP || config.appServer.host;
-};
+function coalesce() {
+    var i, arg;
 
-var getPort = function(){
-    return process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || config.appServer.port;
-};
-
-var getDbConnUr = function(){
-    return process.env.SVNDB_URL || config.dbServer.url;
-};
-
-var getSecretForToken = function(){
-    return process.env.SVN_SECRET_CODE || config.tokenPrivateKey;
-};
-
+    for (i = 0; i < arguments.length; i++) {
+        arg = arguments[i];
+        if (arg !== null && arg !== undefined &&
+            (typeof arg !== 'number' || arg.toString() !== 'NaN')) {
+            return arg;
+        }
+    }
+    return null;
+}
