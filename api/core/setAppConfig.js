5eb7c9a15fcb164f5d5c8b38a065344d3126e437
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2016-07-29 13:54:13 +0000

    code review fix

--- api/core/setAppConfig.js
+++ api/core/setAppConfig.js
@@ -4,12 +4,29 @@
 var config = require("config");
 
 module.exports = function (app) {
-    app.set("host", process.env.OPENSHIFT_NODEJS_IP || process.env.IP || config.appServer.host);
-    app.set("port", process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || config.appServer.port);
-    app.set("connectionString", process.env.SVNDB_URL || config.dbServer.url);
-    app.set("tokenPrivateKey", process.env.SVN_SECRET_CODE || config.tokenPrivateKey);
+    app.set("host", getHost());
+    app.set("port", getPort());
+    app.set("connectionString", getDbConnUr());
+    app.set("tokenPrivateKey", getSecretForToken());
     app.set("tokenExpiresIn", config.tokenExpiresIn);
     app.set("googleOAuthClientID", process.env.SVN_GOOGLE_CLIENTID);
     app.set("googleOAuthClientSecret", process.env.SVN_GOOGLE_CLIENTSECRET);
     app.set("googleOAuthCallbackUrl", process.env.SVN_GOOGLE_CALLBACK_URL);
 };
+
+var getHost = function(){
+    return process.env.OPENSHIFT_NODEJS_IP || process.env.IP || config.appServer.host;
+};
+
+var getPort = function(){
+    return process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || config.appServer.port;
+};
+
+var getDbConnUr = function(){
+    return process.env.SVNDB_URL || config.dbServer.url;
+};
+
+var getSecretForToken = function(){
+    return process.env.SVN_SECRET_CODE || config.tokenPrivateKey;
+};
+
