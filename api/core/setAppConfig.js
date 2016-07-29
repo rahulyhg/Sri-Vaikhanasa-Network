adcd03aa037d05d92dc05a46de3debeb12e8fe1f
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2016-07-29 14:55:29 +0000

    code review fix

--- api/core/setAppConfig.js
+++ api/core/setAppConfig.js
@@ -5,12 +5,14 @@ var config = require("config");
 var coalesce = require("coalescy");
 
 module.exports = function(app) {
-    app.set("host", coalesce(process.env.OPENSHIFT_NODEJS_IP, process.env.IP, config.appServer.host));
-    app.set("port", coalesce(process.env.OPENSHIFT_NODEJS_PORT, process.env.PORT, config.appServer.port));
-    app.set("connectionString", coalesce(process.env.SVNDB_URL, config.dbServer.url));
-    app.set("tokenPrivateKey", coalesce(process.env.SVN_SECRET_CODE, config.tokenPrivateKey));
+    var ENV_VAR = process.env;
+    
+    app.set("host", coalesce(ENV_VAR.OPENSHIFT_NODEJS_IP, ENV_VAR.IP, config.appServer.host));
+    app.set("port", coalesce(ENV_VAR.OPENSHIFT_NODEJS_PORT, ENV_VAR.PORT, config.appServer.port));
+    app.set("connectionString", coalesce(ENV_VAR.SVNDB_URL, config.dbServer.url));
+    app.set("tokenPrivateKey", coalesce(ENV_VAR.SVN_SECRET_CODE, config.tokenPrivateKey));
     app.set("tokenExpiresIn", config.tokenExpiresIn);
-    app.set("googleOAuthClientID", process.env.SVN_GOOGLE_CLIENTID);
-    app.set("googleOAuthClientSecret", process.env.SVN_GOOGLE_CLIENTSECRET);
-    app.set("googleOAuthCallbackUrl", process.env.SVN_GOOGLE_CALLBACK_URL);
+    app.set("googleOAuthClientID", ENV_VAR.SVN_GOOGLE_CLIENTID);
+    app.set("googleOAuthClientSecret", ENV_VAR.SVN_GOOGLE_CLIENTSECRET);
+    app.set("googleOAuthCallbackUrl", ENV_VAR.SVN_GOOGLE_CALLBACK_URL);
 };
