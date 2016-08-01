8283899e8750b8841ddf703c72c442b6aff000f3
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2016-08-01 17:31:53 +0000

    code review fix

--- api/app.js
+++ api/app.js
@@ -3,11 +3,6 @@
 Main entry point for svn api server program
 */
 
-process.env.SVNDB_URL = "mongodb://acharya:acharya@ds023694.mlab.com:23694/achmongodb";
-process.env.SVN_GOOGLE_CLIENTSECRET = "OHYGL260130gWGcqEXD4BTXH";
-process.env.SVN_GOOGLE_CLIENTID = "871728678221-17h5314uvca4i94q63edqu0uoshc8g5j.apps.googleusercontent.com";
-process.env.SVN_GOOGLE_CALLBACK_URL = "https://sri-vaikhanasa-network-acharyarajasekhar.c9users.io/oauth2callback";
-
 // Create express web app and make it globally available
 var app = require("express")();
 global.app = app;
