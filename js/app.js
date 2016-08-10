28a2b7c2b7832fb6683afb1e382ad64c9c3a7312
acharyarajasekhar <acharya.r@live.com>
2016-08-10 23:58:10 +0530

    contact us angular js changes

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var app = angular.module("svnUiApp", []);
+
+app.controller("contactUsCtrl", function ($scope, $http) {
+
+    $scope.SendData = function () {
+        // use $.param jQuery function to serialize data from JSON 
+        // var data = $.param({
+        //     fName: $scope.firstName,
+        //     lName: $scope.lastName
+        // });
+
+        alert("Hello");
+
+        // var config = {
+        //     headers: {
+        //         'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+        //     }
+        // }
+
+        // $http.post('/ServerRequest/PostDataResponse', data, config)
+        //     .success(function (data, status, headers, config) {
+        //         $scope.PostDataResponse = data;
+        //     })
+        //     .error(function (data, status, header, config) {
+        //         $scope.ResponseDetails = "Data: " + data +
+        //             "<hr />status: " + status +
+        //             "<hr />headers: " + header +
+        //             "<hr />config: " + config;
+        //     });
+    };
+
+});
+
