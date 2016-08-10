0cc8fc40aac61187b8cda5926fd91e352cb7b53b
acharyarajasekhar <acharya.r@live.com>
2016-08-10 23:32:39 +0530

    adding transparent nav bar

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+ /*
+  **********************************************************
+  * OPAQUE NAVBAR SCRIPT
+  **********************************************************
+  */
+
+  // Toggle tranparent navbar when the user scrolls the page
+
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
+    {
+        $('.opaque-navbar').removeClass('opaque');        
+    } else {
+        $('.opaque-navbar').addClass('opaque');
+    }
+});
